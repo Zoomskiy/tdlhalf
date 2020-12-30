ce3e1054dca8bcb092ee1a33a32b1439faa04764
Alexander Sindetckiy <zoomskiy@gmail.com>
2020-12-31 01:54:08 +0300

    continue tests need pass last things

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,29 +1,58 @@
-import {TodolistType} from "../App";
+import {FilterValuesType, TodolistType} from "../App";
 import {v1} from "uuid";
 
-type ActionType = {
-    type: string
-    [key: string]: any
+export type RemoveTodolistActionType = {
+    type: "REMOVE-TODOLIST"
+    id: string
 }
+export type AddTodolistActionType = {
+    type: "ADD-TODOLIST"
+    title: string
+}
+export type ChangeTodolistTitleActionType = {
+    type: "CHANGE-TODOLIST-TITLE"
+    id: string
+    title: string
+}
+export type ChangeTodolistFilterActionType = {
+    type: "CHANGE-TODOLIST-FILTER"
+    id: string
+    filter: FilterValuesType
+}
+type ActionsType =
+    RemoveTodolistActionType
+    | AddTodolistActionType
+    | ChangeTodolistFilterActionType
+    | ChangeTodolistTitleActionType
 
-export const todolistReducer = (state: Array<TodolistType>, action: ActionType) => {
+
+export const todolistReducer = (state: Array<TodolistType>, action: ActionsType): Array<TodolistType> => {
     switch (action.type) {
-        case "REMOVE-TODOLIST":
-            let copyState = [...state]
-            copyState = copyState.filter(t => t.id != action.id)
+        case "REMOVE-TODOLIST": {
+            let copyState = state.filter(t => t.id != action.id)
             return copyState
-        case "ADD-TODOLIST":
+        }
+        case "ADD-TODOLIST": {
             return [
                 ...state,
                 {id: v1(), title: action.title, filter: "all"}
             ]
-        case "CHANGE-TODOLIST-TITLE":
-            let copysState = [
-                ...state
-            ]
-            copysState.filter(a => a.id === action.id){
-                 a.title = action.title
         }
+        case "CHANGE-TODOLIST-FILTER" : {
+            let todolist = state.find(t => t.id === action.id)
+            if (todolist) {
+                todolist.filter = action.filter
+            }
+            return [...state]
+        }
+        case "CHANGE-TODOLIST-TITLE": {
+            let TDLItem = state.find(a => a.id === action.id)
+            if (TDLItem) {
+                TDLItem.title = action.title
+                return [...state]
+            }
+        }
+
         default:
             throw new Error("I don't understand this type")
     }
